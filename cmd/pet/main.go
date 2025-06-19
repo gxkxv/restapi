dcfd16989fd30a747d96a751cbe1365ba067b5c6
gxkxv <diaaxe.18@gmail.com>
2025-06-19 18:43:58 +0800

    delete storage.go, create get post patch requests

--- cmd/pet/main.go
+++ cmd/pet/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log/slog"
+	"net/http"
+	"os"
+
 	"github.com/gxkxv/restapi-pet/internal/lib/logger/sl"
 	"github.com/gxkxv/restapi-pet/internal/storage/postgresql"
-	"log/slog"
-	"os"
 
 	"github.com/gxkxv/restapi-pet/internal/config"
 )
@@ -30,7 +34,22 @@ func main() {
 	}
 
 	_ = storage
-	storage.GetInfoFromURL("timur")
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Route("/", func(r chi.Router) {
+		r.Get("/users", postgresql.GetUsers(storage))
+		r.Get("/info", postgresql.GetUser(storage))
+		r.Get("/create", postgresql.CreateUser(storage))
+		r.Post("/create", postgresql.CreateUser(storage))
+		r.Patch("/update/{name}", postgresql.UpdateUser(storage))
+
+	})
+	err = http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Error("error starting server", sl.Err(err))
+	}
 }
 
 func setupLogger(env string) *slog.Logger {
--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/gxkxv/restapi-pet/internal/config"
 	_ "github.com/lib/pq"
+	"log/slog"
 	"net/http"
 )
 
@@ -29,11 +31,12 @@ func New(cfg *config.Config) (*Storage, error) {
 
 	stmt, err := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS users (
+		    id SERIAL,
 			name TEXT,
 			age INTEGER,
 			gender TEXT,
 			nation TEXT,
-			PRIMARY KEY (name, age)
+			PRIMARY KEY (id, name, age)
 		)
 	 `)
 
@@ -105,4 +108,136 @@ func (s *Storage) GetInfoFromURL(name string) error {
 	}
 	_ = stmt
 	return nil
+}
+
+func GetUsers(s *Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		stmt, err := s.db.Prepare("SELECT name, age, gender, nation FROM users")
+		if err != nil {
+			slog.Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		defer stmt.Close()
+		rows, err := stmt.Query()
+		if err != nil {
+			slog.Error(err.Error())
+		}
+		w.Header().Set("Content-Type", "application/json")
+		for rows.Next() {
+			var user User
+			if err := rows.Scan(&user.Name, &user.Age, &user.Gender, &user.Nation); err != nil {
+				slog.Error(err.Error())
+			}
+			if err = json.NewEncoder(w).Encode(user); err != nil {
+				slog.Error(err.Error())
+			}
+		}
+
+		return
+	}
+}
+
+func GetUser(s *Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, "name")
+		if name == "" {
+			http.Error(w, "Missing 'name' query parameter", http.StatusBadRequest)
+			return
+		}
+		stmt, err := s.db.Prepare("SELECT name, age, gender, nation FROM users WHERE name = $1")
+		if err != nil {
+			slog.Error(fmt.Sprintf("prepare user: %w", err))
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		defer stmt.Close()
+		_ = stmt
+		var user User
+		if err := stmt.QueryRow(name).Scan(&user.Name, &user.Age, &user.Gender, &user.Nation); err != nil {
+			slog.Error(fmt.Sprintf("query user: %w", err))
+		}
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(user)
+		if err != nil {
+			slog.Error(fmt.Sprintf("encode user: %w", err))
+		}
+		return
+	}
+}
+
+func CreateUser(s *Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, "name")
+		if name == "" {
+			http.Error(w, "Missing 'name' query parameter", http.StatusBadRequest)
+		}
+		err := s.GetInfoFromURL(name)
+		if err != nil {
+			slog.Error(err.Error())
+		}
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(s)
+		if err != nil {
+			slog.Error(err.Error())
+		}
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+}
+
+func UpdateUser(s *Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, "name")
+		if name == "" {
+			http.Error(w, "Missing 'name' query parameter", http.StatusBadRequest)
+		}
+		var patch struct {
+			Name   *string `json:"name"`
+			Age    *int    `json:"age"`
+			Gender *string `json:"gender"`
+			Nation *string `json:"nation"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&patch); err != nil {
+			slog.Error(err.Error())
+			return
+		}
+		query := "UPDATE users SET "
+		args := []interface{}{}
+		i := 1
+
+		if patch.Name != nil {
+			query += fmt.Sprintf("name = %d ", *patch.Name)
+			args = append(args, *patch.Name)
+			i++
+		}
+		if patch.Age != nil {
+			query += fmt.Sprintf("age = %d ", *patch.Age)
+			args = append(args, *patch.Age)
+			i++
+		}
+		if patch.Gender != nil {
+			query += fmt.Sprintf("gender = '%s' ", *patch.Gender)
+			args = append(args, *patch.Gender)
+			i++
+		}
+		if patch.Nation != nil {
+			query += fmt.Sprintf("nation = '%s' ", *patch.Nation)
+			args = append(args, *patch.Nation)
+			i++
+		}
+		if len(args) == 0 {
+			http.Error(w, "Missing  query parameters", http.StatusBadRequest)
+			return
+		}
+		query = query[:len(query)-1] + fmt.Sprintf(" WHERE name = $%d", i)
+		args = append(args, name)
+		stmt, err := s.db.Exec(query, args...)
+		_ = stmt
+		if err != nil {
+			slog.Error(err.Error())
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
+	}
 }
